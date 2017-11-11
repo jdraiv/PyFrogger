6a3cb6a8154b3f97e7473286951b4c1129539045
Josue Rivera <josuedr11@gmail.com>
2017-11-10 22:16:39 -0600

    Initial commit

---
+++ Frog.py
@@ -0,0 +1,25 @@
+import pygame
+
+import Vars
+
+
+class FrogC:
+    def __init__(self, screen):
+        self.screen = screen
+        # X, Y
+        self.frog_pos = [Vars.screen_width // 2, Vars.screen_height - Vars.frog_size]
+
+    def frog_movement(self, key_char):
+        # list[0] == X Axis
+        # list[1] == Y Axis
+        pos_list = [100, 115]
+        neg_list = [97, 119]
+
+        if key_char in pos_list:
+            self.frog_pos[pos_list.index(key_char)] += Vars.frog_size
+        else:
+            self.frog_pos[neg_list.index(key_char)] -= Vars.frog_size
+
+    def draw_frog(self):
+        pygame.draw.rect(self.screen, [150, 150, 150], [self.frog_pos[0], self.frog_pos[1], Vars.frog_size,
+                                                        Vars.frog_size])
---
+++ Game.py
@@ -0,0 +1,31 @@
+
+import sys, pygame
+
+# Classes
+import Frog
+import Vars
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode([Vars.screen_width, Vars.screen_height])
+        self.frog_class = Frog.FrogC(self.screen)
+
+    def game_loop(self):
+        play = True
+
+        while play:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("Exit")
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    self.frog_class.frog_movement(event.key)
+
+            self.screen.fill([0, 0, 0])
+            self.frog_class.draw_frog()
+
+            pygame.display.update()
+
+
+game = Game()
+game.game_loop()
