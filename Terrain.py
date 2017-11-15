60dd1eeecc67a8e3d016feb49dc23cf420912482
Josue Rivera <josuedr11@gmail.com>
2017-11-15 00:19:23 -0600

    Updated terrains data structure

--- Terrain.py
+++ Terrain.py
@@ -7,7 +7,7 @@ import Vars
 class TerrainC:
     def __init__(self, screen, frog_pos):
         self.screen = screen
-        self.terrain_types = self.initial_terrain()
+        self.terrains = self.initial_terrain()
         self.terrain_colors = [['grass', [92, 249, 87]], ['road', [42, 45, 61]], ['water', [50, 107, 209]]]
         self.frog_pos = frog_pos
         self.last_terrain_index = 0
@@ -17,11 +17,11 @@ class TerrainC:
         t_type = random.randint(0, 2)
 
         for _ in range(4):
-            self.terrain_types.append(t_type)
-            
+            self.terrains.append((t_type, []))
+
     def initial_terrain(self):
-        # Start with two four of grass(type 0)
-        result = [0, 0, 0, 0]
+        # Start with four terrains of grass(type 0)
+        result = [(0, []), (0, []), (0, []), (0, [])]
         """
         Terrain height: Frog width * 4
 
@@ -34,35 +34,40 @@ class TerrainC:
             terrain_t = random.randint(0, 2)
             # Append the initial terrain values
             for _ in range(4):
-                result.append(terrain_t)
+                result.append((terrain_t, []))
 
         return result
+
+    def remove_terrain(self):
+        if self.last_terrain_index > 4:
+            self.terrains = self.terrains[self.last_terrain_index:]
+            self.last_terrain_index = 0
+            print(self.terrains)
 
     def camera_movement(self, key_char):
         if key_char == 119:
             self.last_terrain_index += 1
             self.terrain_c += 1
             # Insert a new terrain into the terrain list
-            if self.terrain_c == 5:
+            if self.terrain_c == 4:
                 self.random_terrain()
                 self.terrain_c = 0
         elif key_char == 115:
             if self.last_terrain_index > 0:
                 self.last_terrain_index -= 1
 
-    def remove_terrain(self):
-        if self.last_terrain_index > 5:
-            self.terrain_types = self.terrain_types[self.last_terrain_index:]
-            self.last_terrain_index = 0
+    def object_spawn(self):
+        pass
 
     def draw_terrain(self):
         # Remove old terrain
         self.remove_terrain()
 
         screen_place = Vars.screen_height - Vars.terrain_height * 4
-        for terrain in range(self.last_terrain_index, len(self.terrain_types)):
-            color = self.terrain_colors[self.terrain_types[terrain]][1]
+        for terrain in range(self.last_terrain_index, len(self.terrains)):
+            color = self.terrain_colors[self.terrains[terrain][0]][1]
             # Draw terrain
             pygame.draw.rect(self.screen, color,
                              [0, screen_place, Vars.screen_width, Vars.frog_size * 4])
             screen_place -= Vars.terrain_height
+
